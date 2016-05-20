edc13a1adea61035a89fe36c01b0cba6847a1c9e
Colin Aulds <caulds989@gmail.com>
2016-05-20 09:08:07 -0500

    playing around

--- apps/app.js
+++ apps/app.js
@@ -1,14 +1,15 @@
+function showResults(results){
+    var html = "";
+    $.each(results, function(index,value){
+        html += '<p>' + value.Title + '</p>';
+    });
+    $('#search-results').html(html);
+}
 $(function(){
     $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
         showResults(data.Search);
     });
 });
-function showResults(results){
-    var html = "";
-    $.each(results, function(index, value) {
-        html + "<p>" + value.Title + "</p>";
-        console.log(value.Title);
-    });
-    $('#search-results').html(html);
-}
+
+
 
